3566b26e0bf92073d8526e741d25e25da4267096
Sajal13 <sajaldas138900@gmail.com>
2026-07-15 12:25:31 +0600

    backend complete, admin setup start

---
+++ apps/admin/types/base.ts
@@ -0,0 +1,14 @@
+export type Size = "sm" | "md" | "base" | "lg";
+export type Color =
+  | "primary"
+  | "secondary"
+  | "tertiary"
+  | "success"
+  | "error"
+  | "info"
+  | "warning"
+  | "orange"
+  | "neutral"
+  | "white";
+export type Shape = "rounded" | "circle";
+export type Variant = "filled" | "outline" | "link";
